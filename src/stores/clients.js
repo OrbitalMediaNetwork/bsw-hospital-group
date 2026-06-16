5898626df6167f50a3b2fa2b312b1139feeae63c
Ali Rasheed <arsalirasheed@gmail.com>
2026-06-16 17:15:48 +0100

    New modules

--- src/stores/clients.js
+++ src/stores/clients.js
@@ -11,14 +11,24 @@ export const clients = [
         thumbURL: "https://bsw-videos-447184491365-eu-west-2-an.s3.eu-west-2.amazonaws.com/epistemic-exploitation/Epistemic+Exploitation+Image.png"
       },
       {
-        title: "Allyship",
-        slug: "allyship",
-        thumbURL: "/images/allyship-thumbnail.png"
+        title: "Building Psychological Safety",
+        slug: "building-psychological-safety",
+        thumbURL: "https://bsw-videos-447184491365-eu-west-2-an.s3.eu-west-2.amazonaws.com/building-psychological-safety/banner.png"
       },
       {
-        title: "Conflict Resolution",
-        slug: "conflict-resolution",
-        thumbURL: "/images/cr-thumbnail.png"
+        title: "EDI - Why Do They Get All the Funding?",
+        slug: "edi-why-dothey-get-all-the-funding",
+        thumbURL: "https://bsw-videos-447184491365-eu-west-2-an.s3.eu-west-2.amazonaws.com/edi-why-dothey-get-all-the-funding/banner.png"
+      },
+      {
+        title: "EDI - It’s a Compliment, Just Ignore It.",
+        slug: "edi-its-a-compliment",
+        thumbURL: "https://bsw-videos-447184491365-eu-west-2-an.s3.eu-west-2.amazonaws.com/edi-its-a-compliment/banner.png"
+      },
+      {
+        title: "EDI - It’s Basically Already Decided.",
+        slug: "edi-its-basically-already-decided",
+        thumbURL: "https://bsw-videos-447184491365-eu-west-2-an.s3.eu-west-2.amazonaws.com/edi-its-basically-already-decided/banner.png"
       }
     ]
   }
